5183a0b7521e9b569648578a3d830aeba575cb14
Daniel Makos <dma@oj.dk>
2024-10-28 13:55:05 +0100

    initial commit + i2c lib

---
+++ SPRO-3/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "i2cmaster.h"
+
+#define CTRLR0 0x07; // address 0x07 corresponds to Control Register 0 
+#define STR0 0x03; // address 0x07 corresponds to Status Register 0 
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
