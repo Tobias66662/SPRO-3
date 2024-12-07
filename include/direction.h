bbc34d98270886886431fbd79638ab77ae8c2faf
Daniel Makos <dma@oj.dk>
2024-12-07 14:33:37 +0100

    testing check_direction

--- include/direction.h
+++ include/direction.h
@@ -2,8 +2,7 @@
 
 #define _DIRECTION_H_
 
-extern bool direction_f;
-void check_direction(); // Note: Function definitions are extern on default.
+bool check_direction(); // Note: Function definitions are extern on default.
 void check_angle(void);
 
 #endif
